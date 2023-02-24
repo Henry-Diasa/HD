965477686c68c6e57e695868494d6000c4caceb6
fuqiang <823342479@qq.com>
2023-02-24 17:53:35 +0800

    feat(部署): 文档部署

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
-let base = '/HD';
+let base = '/HD/';
 let publicPath = '/HD/';
-
+console.log(process.env.SITE_BUILD_ENV, 1111111);
 if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
   base = undefined!;
   publicPath = undefined!;
@@ -10,8 +10,8 @@ export default defineConfig({
   title: 'HD UI',
   mode: 'site',
   outputPath: 'fff', // 输出文件夹
-  exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
-  dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
+  // exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
+  // dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
   base,
   publicPath,
 });
