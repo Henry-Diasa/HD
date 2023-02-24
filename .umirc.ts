aafb18111b2ec2edab3d2b3c7a534fd948289325
Henry-Diasa <Henry-Diasa@users.noreply.github.com>
2023-02-24 10:45:26 +0000

    deploy: e40439ceaa3e70cd90f399d966cc99535bc741cf

--- .umirc.ts
+++ .umirc.ts
@@ -9,7 +9,7 @@ console.log(process.env.SITE_BUILD_ENV, 1111111);
 export default defineConfig({
   title: 'HD UI',
   mode: 'site',
-  outputPath: 'fff', // 输出文件夹
+  // outputPath: 'fff', // 输出文件夹
   // exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
   // dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
   base,
