84b12b03bb692ae6c0fbd4e3f83f52ca65a456b2
diasa <823342479@qq.com>
2023-02-24 21:24:06 +0800

    Update .umirc.ts

--- .umirc.ts
+++ .umirc.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'dumi';
 let base = '/HD/';
-let publicPath = './';
+let publicPath = '/HD/';
 console.log(process.env.SITE_BUILD_ENV, 1111111);
 // if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
 //   base = undefined!;
 //   publicPath = undefined!;
 // }
 export default defineConfig({
-  title: 'HD UI',
+  title: 'HD',
   mode: 'site',
   // outputPath: 'fff', // 输出文件夹
   // exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
