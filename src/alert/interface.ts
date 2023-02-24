61760785ccdfd22db884ac815bf45130d33dce44
Henry-Diasa <Henry-Diasa@users.noreply.github.com>
2023-02-24 10:52:27 +0000

    deploy: c564c0050633422351a23b152aaea3a04a6ff6be

--- src/alert/interface.ts
+++
@@ -1,9 +0,0 @@
-export interface AlertProps {
-  /**
-   * @description Alert的类型
-   * @default 'info'
-   */
-  kind?: 'info' | 'positive' | 'negative' | 'warning';
-}
-
-export type KindMap = Record<Required<AlertProps>['kind'], string>;
