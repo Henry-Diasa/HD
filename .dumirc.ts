d3468de7bd299662d9437b44233a334b22b88d49
付强 <fuqiang1@CN0214003515M.lan>
2023-02-24 22:43:17 +0800

    feat(部署): 部署文档

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'dumi';
-let base = '/HD';
-let publicPath = '/HD/';
-console.log(process.env.SITE_BUILD_ENV, 1111111);
-// if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
-//   base = undefined!;
-//   publicPath = undefined!;
-// }
+let base;
+let publicPath;
+if (process.env.NODE_ENV === 'production' && process.env.SITE_BUILD_ENV !== 'PREVIEW') {
+  base = '/HD';
+  publicPath = '/HD/';
+}
 export default defineConfig({
   title: 'HD',
-  mode: 'site',
   outputPath: 'doc-dist', // 输出文件夹
   // exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
   // dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
