b745982fa39d5ab2785ba429fca6fa6f85f08e6a
fuqiang <823342479@qq.com>
2023-02-28 16:53:50 +0800

    feat(增加编译): js css等编译流程

---
+++ lib/alert/index.d.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import type { AlertProps } from './interface';
+declare const Alert: React.FC<PropsWithChildren<AlertProps>>;
+export default Alert;
